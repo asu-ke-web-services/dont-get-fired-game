3f12519272239d6439898e004e18a02828cfedc0
Ivan Montiel <idmontie@asu.edu>
2015-09-23 13:09:21 -0700

    Add coveralls to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,55 @@
 var gulp    = require('gulp');
+var runSequence = require('run-sequence');
+
 var jasmine = require('gulp-jasmine');
 var cover   = require('gulp-coverage');
+var coveralls = require('gulp-coveralls');
 
 var options = {
-  testPaths : [
-    'tests/*.js',
-    'tests/**/*.js',
-  ],
+  testPaths : {
+    unit : [
+      'tests/unit/*.js',
+      'tests/unit/**/*.js',
+    ],
+    spec : [
+      'tests/spec/*.js',
+      'tests/spec/**/*.js',
+    ],
+    integration : [
+      'tests/integration/*.js',
+      'tests/integration/**/*.js',
+    ]
+  },
   filePaths : [
-    'framework/*.js',
-    'framework/**/*.js',
-    'src/*.js',
-    'src/**/*.js'
+    core : [
+      'src/core/*.js',
+      'src/core/**/*.js'
+    ],
+    framework : [
+      'src/framework/*.js',
+      'src/framework/**/*.js'
+    ],
   ],
   jasmine : {
     includeStackTrace : true,
     verbose: true
   }
 }
+gulp.task('test', function () {
+   return gulp.src( options.testPaths.spec )
+          .pipe(cover.instrument({
+            pattern: options.filePaths.core,
+            debugDirectory: 'debug'
+          }))
+          .pipe( jasmine( options.jasmine ) )
+          .pipe(cover.gather())
+          .pipe(cover.format({
+            reporter: 'lcov'
+          }))
+          .pipe(coveralls())
+          .pipe(gulp.dest('coverage'));
+});
 
-gulp.task('default', function () {
-  return gulp.src( options.testPaths )
-             .pipe(cover.instrument({
-               pattern: options.filePaths,
-               debugDirectory: 'debug'
-             }))
-             .pipe( jasmine( options.jasmine ) )
-             .pipe(cover.gather())
-             .pipe(cover.format())
-             .pipe(gulp.dest('coverage'));
+gulp.task('default', function ( cb ) {
+  runSequence( 'test', 'build', cb );
 });
