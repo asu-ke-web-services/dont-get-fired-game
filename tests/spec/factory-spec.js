fde4b0d052b89907346d588a145df2422609254c
Ivan Montiel <idmontie@asu.edu>
2015-10-26 15:40:51 -0700

    Update factory-spec.js

--- tests/spec/factory-spec.js
+++ tests/spec/factory-spec.js
@@ -8,11 +8,17 @@ describe( 'a factory', function() {
     factory = new Factory( {
       name: 'Toy Factory'
     } );
+    
+    factoryNoOptions = new Factory();
   } );
 
   it( 'has a name', function() {
     expect( factory.name ).toBeDefined();
     expect( factory.name ).toBe( 'Toy Factory' );
+  } );
+  
+  it( 'does not require options', function() {
+    expect( factoryNoOptions ).toBeUndefined();
   } );
 } );
 
