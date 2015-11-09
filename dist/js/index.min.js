6a1d4b16a70150860996091ffdf8b2a36a5c57b2
= <=>
2015-11-09 14:15:18 -0700

    Added more to the ui_prototype

--- dist/js/index.min.js
+++ dist/js/index.min.js
@@ -86,7 +86,15 @@
 	      class: 'factory_entity',
 	      html: 'Factory'
 	    } );
-	$gameContainer.append( $factoryEntity );
+	/**Note to whoever ends up writing code to link to the UI, I can leave these
+	 * as divs where you can replace all the text with something like "Funds "+ object.funds;"
+	 * or I could add a span so you would just have to set the value of the span inside of
+	 * these divs**/
+	var $quarterYear = '<div>Quarter 1 / Year 0 [00]</div>';
+	var $funds = '<div>Funds $000000 - 0000 / Quarter</div>';
+	var $perception = '<div>Perception 00</div>';
+	var $goals = '<div>Goals: </div>';
+	$gameContainer.append( $factoryEntity, '<hr>', $quarterYear, $funds, $perception );
 
 
 
