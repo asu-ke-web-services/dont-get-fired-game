097bfdc86d91b04b500cd3aab29093aa39150f10
= <=>
2015-11-09 15:50:23 -0700

    Add spans with ids to be referenced easily by jquery in UI

--- src/scaffolding/ui-prototype.js
+++ src/scaffolding/ui-prototype.js
@@ -4,12 +4,16 @@ var $factoryEntity = $( '<div />',
       class: 'factory_entity',
       html: 'Factory'
     } );
-/**Note to whoever ends up writing code to link to the UI, I can leave these
- * as divs where you can replace all the text with something like "Funds "+ object.funds;"
- * or I could add a span so you would just have to set the value of the span inside of
- * these divs**/
-var $quarterYear = '<div>Quarter 1 / Year 0 [00]</div>';
-var $funds = '<div>Funds $000000 - 0000 / Quarter</div>';
-var $perception = '<div>Perception 00</div>';
-var $goals = '<div>Goals: </div>';
-$gameContainer.append( $factoryEntity, '<hr>', $quarterYear, $funds, $perception );
+
+var $quarterYear = '<div>Quarter <span id="quarterValue">1</span> / ' +
+    'Year <span id="yearValue">0</span> ' +
+    '[<span id="timeProgressValue">1</span>]</div>';
+
+var $funds = '<div>Funds $<span id="totalFundsValue">000000</span> - ' +
+    '<span id="fundsLostPerQuarterValue">0000</span> / Quarter</div>';
+
+var $perception = '<div>Perception <span id="perceptionValue">1</span>00</div>';
+
+var $goals = '<div>Goals: <span id="goalsValue">none</span></div>';
+
+$gameContainer.append( $factoryEntity, '<hr>', $quarterYear, $funds, $perception, $goals );
