eeaa26826ea7548dc8221c1bd064d03af901adcf
tuyenho <phongvanlongthan@gmail.com>
2015-11-16 15:11:11 -0700

    Fix bugs

--- src/core/entities/material.js
+++ src/core/entities/material.js
@@ -4,8 +4,8 @@
 var Material = function( options ) {
   options = options || {};
   this.materialName = options.materialName ? options.materialName : '';
-  this.cost = option.cost ? options.cost : '';
-  this.wastes = option.wastes ? options.wastes : '';
+  this.cost = options.cost ? options.cost : '';
+  this.wastes = options.wastes ? options.wastes : '';
 };
 module.exports = Material;
 
