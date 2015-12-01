fb1a14be587c2969c7e024965f7ff5e761e133aa
Ivan Montiel <idmontie@asu.edu>
2015-12-01 15:14:29 -0700

    Fix lint and make a settings file on travis

--- tests/spec/quarter-perception-strategy-spec.js
+++ tests/spec/quarter-perception-strategy-spec.js
@@ -1,7 +1,7 @@
 var QuarterPerceptionStrategy = require( __core + 'strategies/quarter-perception-strategy' );
 
 describe( 'the quarter perception strategy', function() {
-  it( 'produces a poor perception given a high waste to items ratio', function () {
+  it( 'produces a poor perception given a high waste to items ratio', function() {
     var quarter = {
       factoryWaste: 1000,
       itemsMade: 1,
@@ -9,14 +9,14 @@ describe( 'the quarter perception strategy', function() {
       itemsSold: 1
     };
 
-    var strategy = new QuarterPerceptionStrategy(quarter);
+    var strategy = new QuarterPerceptionStrategy( quarter );
 
     var result = strategy.execute();
 
-    expect(result).toBeLessThan(5);
+    expect( result ).toBeLessThan(5);
   } );
 
-  it( 'produces a good perception given a low waste to items ratio', function () {
+  it( 'produces a good perception given a low waste to items ratio', function() {
     var quarter = {
       factoryWaste: 1,
       itemsMade: 10,
@@ -24,10 +24,10 @@ describe( 'the quarter perception strategy', function() {
       itemsSold: 10
     };
 
-    var strategy = new QuarterPerceptionStrategy(quarter);
+    var strategy = new QuarterPerceptionStrategy( quarter );
 
     var result = strategy.execute();
 
-    expect(result).toBeGreaterThan(5);
+    expect( result ).toBeGreaterThan( 5 );
   } );
 } );
