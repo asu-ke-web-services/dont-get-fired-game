da1a36e9bc1e48efffad41a9f825971fccc8beba
= <davidsmall0305@gmail.com>
2016-02-15 21:32:49 -0700

    Deleted dist folder and modified advisorlist component to use props

--- client/components/game/home/advisorPanel.jsx
+++ client/components/game/home/advisorPanel.jsx
@@ -1,27 +1,13 @@
 import React from 'react';
 
-var testList = [{
-  name:"Dane",
-  dialog:"Business is booming!",
-},
-  {
-    name:"Ashley",
-    dialog:"Can't we produce less waste somehow?",
-  },
-  {
-    name:"Gary",
-    dialog:"Oi! YOU'RE DRIVING US INTO THE GROUND!!"
-  }
-];
 export default React.createClass({
-  testList,
   render() {
     return (
         <div>
           <h2>Your Advisors</h2>
           <ul>
-            {this.testList.map(function(listvalue){
-              return <li>{listvalue.name} - {listvalue.dialog}</li>;
+            {this.props.advisorList.map(function(advisor){
+              return <li>{advisor.name} - {advisor.dialog}</li>;
             })}
           </ul>
         </div>
