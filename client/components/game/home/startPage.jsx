f147e6998382cecdee7f0077523cc56acbadb0cd
xyan12345 <xuefengyan@yahoo.com>
2016-02-17 17:18:57 -0700

    startpage modified

--- client/components/game/home/startPage.jsx
+++ client/components/game/home/startPage.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
 export default React.createClass({
-
+    handleGame(e){     
+        e.preventDefault();
+        //will add the button handler to new game later
+    },
+    handleCredit(e){
+        e.prevetDefault();
+        //will add the button handler to credits later
+    },
   render() {
     return (
         <div>
           <h1>Dont Get Fired</h1>
-
-            <button>new game</button>
+            <button onClick= {this.handlePlay}>new game</button>
+            <button onClick= {this.handleCredit}>credits</button>
         </div>
     );
   }
