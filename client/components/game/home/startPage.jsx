2a65abc8fab9dc052beb15b6809a65f05246082f
xyan12345 <xuefengyan@yahoo.com>
2016-02-17 17:08:17 -0700

    startPage

---
+++ client/components/game/home/startPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default React.createClass({
+
+  render() {
+    return (
+        <div>
+          <h1>Dont Get Fired</h1>
+
+            <button>new game</button>
+        </div>
+    );
+  }
+});
