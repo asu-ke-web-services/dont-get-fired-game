6e50568fb6fa3626c6f714a4e00e75e57d60b48c
rover2341 <Hulk2340>
2016-02-17 23:09:16 -0700

    Added Programs, Program, Program List and Program List Item Mannualy Tested: Program List Item, and Program before adding data (this..) Unable to test Program List, and Programs. Progrma List was based on Issue 74

---
+++ client/components/game/programs/programs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ProgramList from 'program_list';
+export default React.createClass({
+    render() {
+        return (
+            <div>
+                <ProgramList  />
+            <div/>
+        );
+    }
+});
