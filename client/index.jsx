6c2937e8a7bab63dc4e3d5808edd5088d8498f7f
Ivan Montiel <idmontie@asu.edu>
2016-02-18 18:30:35 -0700

    Fix index

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {default as Game} from './components/game/home/home.jsx';
+import {default as Game} from './containers/game/game.jsx';
 
 Meteor.startup(function () {
   ReactDOM.render( <Game />, document.getElementById('render-target'));
