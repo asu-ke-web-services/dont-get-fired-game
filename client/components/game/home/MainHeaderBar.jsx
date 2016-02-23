e48dbe0e0079776a36771a53dd105d11d0fdf75b
Ivan Montiel <idmontie@asu.edu>
2016-02-23 14:29:00 -0700

    Fix spacing in main header bear

--- client/components/game/home/MainHeaderBar.jsx
+++ client/components/game/home/MainHeaderBar.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
 export default React.createClass({
-    render() {
-        return (
-            <div>
-                <div style={{display: "inline-block", marginRight: "30px"}}>Capital: {this.prop.capital} </div>
-                <div style={{display: "inline-block", marginRight: "30px"}}>Cash Flow: {this.prop.cashFlow}</div>
-                <div style={{display: "inline-block"}}>Quarter: {this.prop.quarter} of {this.prop.totalQuarterCount}</div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div style={{display: "inline-block", marginRight: "30px"}}>
+          Capital: {this.props.capital}
+        </div>
+        <div style={{display: "inline-block", marginRight: "30px"}}>
+          Cash Flow: {this.props.cashFlow}
+        </div>
+        <div style={{display: "inline-block"}}>
+          Quarter: {this.props.quarter} of {this.props.totalQuarterCount}
+        </div>
+      </div>
+    );
+  }
 });
