ed13fc442133b0a8aab088572fcb464ec62b809f
Tuyen Ho <Tuyen Ho>
2016-02-22 21:18:11 -0700

    Fixed Travis Lint

--- client/components/game/setup/setup.jsx
+++ client/components/game/setup/setup.jsx
@@ -10,7 +10,7 @@ export default React.createClass({
 
   handleRandomize(e) {
     e.preventDefault();
-    
+
     // TODO change view state!
     GameActions.create();
   },
@@ -18,7 +18,7 @@ export default React.createClass({
   handleStart(e) {
     e.preventDefault();
 
-    GameActions.create(); 
+    GameActions.create();
   },
 
   render() {
--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import {default as Game} from './containers/game/game.jsx';
+// import {default as Game} from './containers/game/game.jsx';
 
 import {default as Quater} from './components/game/home/eventPage/eventPage.jsx';
 
