aeaf35b577aef267c4cb9c921d6ff8d88441f323
Ivan Montiel <idmontie@asu.edu>
2016-02-23 13:24:02 -0700

    Fix whitespace

--- client/components/game/home/startPage.jsx
+++ client/components/game/home/startPage.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
 export default React.createClass({
-  handleGame(e){     
+  handleGame(e) {
     e.preventDefault();
     // will add the button handler to new game later
   },
-  handleCredit(e){
+  handleCredit(e) {
     e.prevetDefault();
     // will add the button handler to credits later
   },
   render() {
-  return (
-    <div>
-      <h1>Dont Get Fired</h1>
-      <button onClick= {this.handlePlay}>new game</button>
-      <button onClick= {this.handleCredit}>credits</button>
-    </div>
-  );
+    return (
+      <div>
+        <h1>Dont Get Fired</h1>
+        <button onClick= {this.handlePlay}>new game</button>
+        <button onClick= {this.handleCredit}>credits</button>
+      </div>
+    );
   }
 });
