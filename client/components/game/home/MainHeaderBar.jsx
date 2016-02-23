68670d8b0a791a5ae807b8a222ba342c4b0f08d3
Ivan Montiel <idmontie@asu.edu>
2016-02-23 14:49:52 -0700

    Replace double quotes with single quotes

--- client/components/game/home/MainHeaderBar.jsx
+++ client/components/game/home/MainHeaderBar.jsx
@@ -4,13 +4,13 @@ export default React.createClass({
   render() {
     return (
       <div>
-        <div style={{display: "inline-block", marginRight: "30px"}}>
+        <div style={{display: 'inline-block', marginRight: '30px'}}>
           Capital: {this.props.capital}
         </div>
-        <div style={{display: "inline-block", marginRight: "30px"}}>
+        <div style={{display: 'inline-block', marginRight: '30px'}}>
           Cash Flow: {this.props.cashFlow}
         </div>
-        <div style={{display: "inline-block"}}>
+        <div style={{display: 'inline-block'}}>
           Quarter: {this.props.quarter} of {this.props.totalQuarterCount}
         </div>
       </div>
