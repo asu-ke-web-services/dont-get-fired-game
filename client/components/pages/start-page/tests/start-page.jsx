bbebbbe6765180bca5e83d70d622a15cca28c270
Ivan Montiel <idmontie@asu.edu>
2016-03-01 10:39:34 -0700

    Fix page test

--- client/components/pages/start-page/tests/start-page.jsx
+++ client/components/pages/start-page/tests/start-page.jsx
@@ -3,12 +3,12 @@ import {expect} from 'chai';
 import {shallow} from 'enzyme';
 const {describe, it} = global;
 
-import { default as Page } from '../start-page.jsx';
+import { StartPage } from '../start-page.jsx';
 
-describe('A start-page has buttons', () => {
+describe('The Start Page', () => {
 
   it( 'has new game button', () => {
-    const wrapper = shallow(<Page />);
+    const wrapper = shallow(<StartPage />);
     expect(wrapper.contains('new game')).to.equal(true);
   });
 });
