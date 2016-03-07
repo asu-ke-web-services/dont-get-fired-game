1eb150597855c584cf4ad14e037e8bd398c832ad
Ivan Montiel <idmontie@asu.edu>
2016-03-07 11:33:17 -0700

    Fix lint issues

--- client/data/programs.js
+++ client/data/programs.js
@@ -3,37 +3,37 @@ export default [
     name: 'Quality Control',
     description: `Add quality control checking to your business via Six Sigma.`,
     programImage: '',
-    initialCost: function (game) {
+    initialCost(game) {
       return game.company.numberOfFactories * 10000;
     },
-    quarterCost: function (game) {
+    quarterCost(game) {
       return game.company.numberOfFactories * 1000;
     },
-    initialEffects: function () {
+    initialEffects() {
       return {
         companySatisfaction: 0.1,
         customerSatisfaction: 0.10,
         averageFactoryEfficiency: -0.1
-      }
+      };
     },
   },
   {
     name: 'Recycling',
     description: '',
     programImage: '',
-    initialCost: function (game) {
+    initialCost(game) {
       return game.company.numberOfFactories * 5000;
     },
-    quarterCost: function (game) {
+    quarterCost(game) {
       return game.company.numberOfFactories * 3000;
     },
-    initialEffects: function () {
+    initialEffects() {
       return {
         companySatisfaction: 0.1,
         customerSatisfaction: 0.2
       };
     },
-    quarterEffects: function () {
+    quarterEffects() {
       return {
         customerSatisfaction: 0.1
       };
@@ -43,20 +43,20 @@ export default [
     name: 'Use Clean Energy',
     description: 'Get the energy to power your factories from clean energy sources only',
     programImage: '',
-    initialCost: function (game) {
+    initialCost(game) {
       return game.company.numberOfFactories * 50000;
     },
-    quarterCost: function (game) {
+    quarterCost(game) {
       return game.company.numberOfFactories * 10000;
     },
-    initialEffects: function () {
+    initialEffects() {
       return {
         companySatisfaction: -0.2,
         customerSatisfaction: 0.5,
         averageFactoryEfficiency: -0.1
       };
     },
-    quarterEffects: function () {
+    quarterEffects() {
       return {
         customerSatisfaction: 0.2
       };
@@ -66,19 +66,19 @@ export default [
     name: 'Catered Lunches',
     description: '',
     programImage: '',
-    initialCost: function (game) {
+    initialCost(game) {
       return game.company.numberOfFactories * 10000;
     },
-    quarterCost: function (game) {
+    quarterCost(game) {
       return game.company.numberOfFactories * 10000;
     },
-    initialEffects: function () {
+    initialEffects() {
       return {
         companySatisfaction: 0.3,
         averageFactoryEfficiency: 0.05
       };
     },
-    quarterEffects: function () {
+    quarterEffects() {
       return {
         companySatisfaction: 0.1
       };
@@ -89,13 +89,13 @@ export default [
     description: '',
     programImage: '',
     allowMultiple: true,
-    initialCost: function (game) {
+    initialCost(game) {
       return game.industry.initialCostPerFactory;
     },
-    quarterCost: function (game) {
+    quarterCost(game) {
       return game.industry.quarterCostPerFactory;
     },
-    initialEffects: function () {
+    initialEffects() {
       return {
         companySatisfaction: 0.1,
         averageFactoryEfficiency: -0.05
