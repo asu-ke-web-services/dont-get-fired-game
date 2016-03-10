aa0178cf74c4f2724d26040ab2ca577e126677d0
xyan12345 <xuefengyan@yahoo.com>
2016-03-10 09:47:04 -0700

    fix company name

--- client/components/game/company-name/company－name.jsx
+++ client/components/game/company-name/company－name.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default React.createClass({
   render() {
     return (
-      <div className="company_name">
+      <div className="company－name">
         <h>{this.props.companyName}</h>
       </div>
     );
