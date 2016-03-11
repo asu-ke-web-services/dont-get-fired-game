dc5599eded8403898ecc41d11f78034db329fd10
xyan12345 <xuefengyan@yahoo.com>
2016-03-11 11:58:45 -0700

    MainSideBar fixed

--- client/components/game/MainSideBar/main-side-bar.jsx
+++ client/components/game/MainSideBar/main-side-bar.jsx
@@ -1,19 +1,21 @@
-import { next-quarter-button } from '../../../components/game/sidebar/next-quarter-button/next-quarter-button';
-import { advisor-panel } from '../../../components/game/advisor-panel/advisor-panel';
-import {company-name} from '../../../components/game/company-name/company-name';
+import React from 'react';
+
+import { mainSideBarButton} from '../../../sidebar/next-quarter-button/next-quarter-button';
+import { advisorPanel } from '../../../advisor-panel/advisor-panel';
+import {companyName} from '../../../company-name/company-name';
 
 export default React.createClass({
   render() {
     return (
       <div className="main-side-bar">
         <div className="company-name">
-          <company-name/>
+          <companyName/>
         </div>
         <div className="advisor-panel">
-          <advisor-panel/>
+          <advisorPanel/>
         </div>
         <div className="next-quarter-button">
-          <next-quarter-button/>
+          <mainSideBarButton/>
         </div>
       </div>
 
