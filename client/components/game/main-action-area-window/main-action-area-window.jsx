d11beb88e7f25f28b7bc63cd4337a44319ff0082
rover2341 <Hulk2340>
2016-03-12 13:29:44 -0700

    Fixed Test for main-action-area-window, fired main-action-area-window

--- client/components/game/main-action-area-window/main-action-area-window.jsx
+++ client/components/game/main-action-area-window/main-action-area-window.jsx
@@ -3,19 +3,28 @@ import React from 'react';
 import { default as ProgramList } from '../program-list/program-list.jsx';
 import { default as Program } from '../program/program.jsx';
 export default React.createClass({
+  renderProgramGroup() {
+    if (this.props.programs === null) {
+      return (
+        <h4>No Programs Exist</h4>
+      );
+    }
+    if (this.props.selectedProgram !== null) {
+      return (
+        <Program program={this.props.selectedProgram} />
+      );
+    }
+    if (this.props.selectedProgram === null) {
+      return (
+        <ProgramList programs={this.props.programs}/>
+      );
+    }
+  },
   render() {
     return (
       <div className="main-action-area-window">
-        <h4>Things To Do</h4>
-
-        if(this.props.selectedProgram == null){
-          <ProgramList programs={this.props.programs}/>
-        }
-        else {
-          <Program program={this.props.selectedProgram} />
-        }
-
-        </div>
+        {this.renderProgramGroup()}
+      </div>
      );
   }
 });
--- client/components/game/main-action-area-window/tests/main-action-aera-window.jsx
+++ client/components/game/main-action-area-window/tests/main-action-aera-window.jsx
@@ -1,55 +1,79 @@
 import React from 'react';
 import {expect} from 'chai';
-import {shallow} from 'enzyme';
+import {shallow,render} from 'enzyme';
 const {describe, it} = global;
 
 import { default as MainActionAreaWindow } from '../main-action-area-window.jsx';
 
 
+describe('No Programs Exist', () => {
+
+  const selectedProgram = null;
+  const programs = null;
+
+  it( 'has a name', () => {
+    const wrapper = shallow(<MainActionAreaWindow selectedProgram={selectedProgram}
+                                                  programs={programs}/>);
+    expect(wrapper.contains('No Programs Exist')).to.equal(true);
+  });
+});
+
 describe('A Program Selected', () => {
+
   const selectedProgram = {
     name: 'Save the whales',
     shortDescription: 'Save the whales is da best thing ever',
     cost: 100,
-    additional: 'Save the whales will make you a better person'
+    additional: 'Save the whales will make you a better person',
+    image: 'abc',
+    actionPointValue: 'egh'
   };
   const programs = [ {
     name: 'Save the whales',
     shortDescription: 'Save the whales is da best thing ever',
     cost: 100,
-    additional: 'Save the whales will make you a better person'
+    additional: 'Save the whales will make you a better person',
+    image: 'abc',
+    actionPointValue: 'egh'
   }, {
-    name: 'Save the cats',
-    shortDescription: 'Save the cats is da best thing ever',
+    name: 'Save the Bears',
+    shortDescription: 'Save the bears is da best thing ever',
     cost: 100,
-    additional: 'Save the cats will make you a better person'
+    additional: 'Save the bears will make you a better person',
+    image: 'abc',
+    actionPointValue: 'egh'
   } ];
+
   it( 'has a name', () => {
-    const wrapper = shallow(<MainActionAreaWindow programs={programs}
-    selectedProgram={selectedProgram} />);
-    expect(wrapper.contains('Save the whales')).to.equal(false);
+    const wrapper = render(<MainActionAreaWindow selectedProgram={selectedProgram}
+                                                 programs={programs}/>);
+    // console.log(wrapper.text());
+    expect(wrapper.text()).to.contain('Save the whales');
   });
-
 });
-
 describe('A Program Is Not Selected', () => {
-  const selectedProgram = null;
 
+  const selectedProgram = null;
   const programs = [ {
     name: 'Save the whales',
     shortDescription: 'Save the whales is da best thing ever',
     cost: 100,
-    additional: 'Save the whales will make you a better person'
+    additional: 'Save the whales will make you a better person',
+    image: 'abc',
+    actionPointValue: 'egh'
   }, {
-    name: 'Save the cats',
-    shortDescription: 'Save the cats is da best thing ever',
+    name: 'Save the Bears',
+    shortDescription: 'Save the bears is da best thing ever',
     cost: 100,
-    additional: 'Save the cats will make you a better person'
+    additional: 'Save the bears will make you a better person',
+    image: 'abc',
+    actionPointValue: 'egh'
   } ];
 
-  it( 'has a name', () => {
-    const wrapper = shallow(<MainActionAreaWindow
-    programs={programs} selectedProgram={selectedProgram} />);
-    expect(wrapper.contains('Save the cats')).to.equal(false);
+  it( 'list item has a name', () => {
+    const wrapper = render(<MainActionAreaWindow selectedProgram={selectedProgram}
+                                                 programs={programs}/>);
+    // console.log(wrapper.text());
+    expect(wrapper.text()).to.contain('Save the Bears');
   });
 });
