19e10f3355de6d08ca52ee15859c062597d5b8c9
xyan12345 <xuefengyan@yahoo.com>
2016-03-22 21:50:12 -0700

    add cs-program data

--- client/data/cs-programs.js
+++ client/data/cs-programs.js
@@ -1,3 +1,98 @@
-/**
- * Created by marks on 3/21/2016.
- */
+[{
+     "name":"open new branch",
+     "description":"new branch company in Los Angeles",
+     "onBuyActionPoints":2,
+     "onBuyCaptial ":3,
+     "onBuySatisfaction":4,
+     "onQuarterActionPoints":5,
+     "onQuarterCaptial":4,
+     "onQuarterSatisfaction":5,
+     "image":"/icons/catering-icon.png",
+     "isPurchased": false
+ },
+ {
+     "name":"Sell a branch company",
+     "description":"Sell a non-profit branch company",
+     "onBuyActionPoints":2,
+     "onBuyCaptial ":3,
+     "onBuySatisfaction":4,
+     "onQuarterActionPoints":5,
+     "onQuarterCaptial":5,
+     "onQuarterSatisfaction":6,
+     "image":"/icons/catering-icon.png",
+     "isPurchased": false
+ },
+ {
+     "name":"Hire new employee",
+     "description":"hire more people to work in company",
+     "onBuyActionPoints":2,
+     "onBuyCaptial ":3,
+     "onBuySatisfaction":4,
+     "onQuarterActionPoints":2,
+     "onQuarterCaptial":3,
+     "onQuarterSatisfaction":5,
+     "image":"/icons/catering-icon.png",
+     "isPurchased": false
+ },
+ {
+     "name":"fire employee",
+     "description":"fire some employee to have maximum profit ",
+     "onBuyActionPoints":2,
+     "onBuyCaptial ":3,
+     "onBuySatisfaction":3,
+     "onQuarterActionPoints":4,
+     "onQuarterCaptial":1,
+     "onQuarterSatisfaction":5,
+     "image":"/icons/catering-icon.png",
+     "isPurchased": false
+ },
+{
+     "name":"promote a CIO",
+     "description":"Promote a new CIO ",
+     "onBuyActionPoints":2,
+     "onBuyCaptial ":3,
+     "onBuySatisfaction":1,
+     "onQuarterActionPoints":2,
+     "onQuarterCaptial":2,
+     "onQuarterSatisfaction":3,
+     "image":"/icons/catering-icon.png",
+     "isPurchased": false
+ },
+{
+     "name":"New develop team",
+     "description":"open a new develop team to have better product",
+     "onBuyActionPoints":2,
+     "onBuyCaptial ":2,
+     "onBuySatisfaction":4,
+     "onQuarterActionPoints":2,
+     "onQuarterCaptial":4,
+     "onQuarterSatisfaction":2,
+     "image":"/icons/catering-icon.png",
+     "isPurchased": false
+ },
+{
+     "name":"rise salary",
+     "description":"rise employee's salary",
+     "onBuyActionPoints":2,
+     "onBuyCaptial ":3,
+     "onBuySatisfaction":4,
+     "onQuarterActionPoints":2,
+     "onQuarterCaptial":3,
+     "onQuarterSatisfaction":3,
+     "image":"/icons/catering-icon.png",
+     "isPurchased": false
+ },
+{
+     "name":"reduce wages",
+     "description":"reduce wages to have more profit",
+     "onBuyActionPoints":2,
+     "onBuyCaptial ":2,
+     "onBuySatisfaction":4,
+     "onQuarterActionPoints":2,
+     "onQuarterCaptial":3,
+     "onQuarterSatisfaction":2,
+     "image":"/icons/catering-icon.png",
+     "isPurchased": false
+ }
+
+]
