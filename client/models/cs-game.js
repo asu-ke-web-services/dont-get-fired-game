8d2a426dd8e88542bc68dad9fe502c74b8feb130
= <davidsmall0305@gmail.com>
2016-03-24 15:43:00 -0700

    Fix linting issues

--- client/models/cs-game.js
+++ client/models/cs-game.js
@@ -21,7 +21,8 @@ export default class CSGame {
 
   }
   SampleGame() {
-    this.companyName = this.getRandomOption(['Solar Tech','Freddy and Friend\'s Discount Cars','David\'s Bucket O\' Chicken', 'Dane and Ash Guitars', 'Gary\'s General Store']);
+    this.companyName = this.getRandomOption([ 'Solar Tech','Freddy and Friend\'s Discount Cars',
+      'David\'s Bucket O\' Chicken', 'Dane and Ash Guitars', 'Gary\'s General Store' ]);
     this.goals = this.getRandomOption([
       new CSGoal('Get Rich','Have $1000 to win.',1000, 0),
       new CSGoal('Oh-So Satisfying!','Have 100 Satisfaction to win.',0, 100),
@@ -39,12 +40,12 @@ export default class CSGame {
             1, 1, 1)
     );
     this.advisors = Advisers;
-    this.totalQuarters = this.getRandomOption([8,10,15]);
+    this.totalQuarters = this.getRandomOption([ 8,10,15 ]);
     this.currentQuarter = 0;
-    this.capital = this.getRandomOption([50,100,200]);
-    this.capitalPerQuarter = this.getRandomOption([15,25,30]);
-    this.actions = this.getRandomOption([1,2,3]);
-    this.actionsPerQuarter = this.getRandomOption([1,2,3]);
+    this.capital = this.getRandomOption([ 50,100,200 ]);
+    this.capitalPerQuarter = this.getRandomOption([ 15,25,30 ]);
+    this.actions = this.getRandomOption([ 1,2,3 ]);
+    this.actionsPerQuarter = this.getRandomOption([ 1,2,3 ]);
     this.satisfactionPerQuarter = [];
   }
 
@@ -54,8 +55,8 @@ export default class CSGame {
     this.actions -= program.onBuyActionPoints;
   }
 
-  //pass in an array and return 1 object from that array randomly
-  getRandomOption(list){
+  // pass in an array and return 1 object from that array randomly
+  getRandomOption(list) {
     var randomIndex = Math.floor(Math.random() * list.length);
     return list[randomIndex];
   }
