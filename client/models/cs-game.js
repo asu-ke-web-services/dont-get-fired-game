30ea76c8b6d55075b47ac8c6c0e57af18a8efa37
David <davidsmall0305@gmail.com>
2016-03-23 22:03:13 -0700

    Added randomization for game settins

--- client/models/cs-game.js
+++ client/models/cs-game.js
@@ -21,8 +21,12 @@ export default class CSGame {
 
   }
   SampleGame() {
-    this.companyName = 'Sample Company';
-    this.goals = new CSGoal('Get Rich','Have 1000$ To win.',1000, 0);
+    this.companyName = this.getRandomOption(['Solar Tech','Freddy and Friend\'s Discount Cars','David\'s Bucket O\' Chicken', 'Dane and Ash Guitars', 'Gary\'s General Store']);
+    this.goals = this.getRandomOption([
+      new CSGoal('Get Rich','Have $1000 to win.',1000, 0),
+      new CSGoal('Oh-So Satisfying!','Have 100 Satisfaction to win.',0, 100),
+      new CSGoal('Jack Of All Trades','Have $1000 and 100 Satisfaction to win.',1000, 100)
+    ]);
     this.programs = [];
     this.programs.push(
       new CSProgram( 'Program Sample', 'A Great Deal',
@@ -35,12 +39,12 @@ export default class CSGame {
             1, 1, 1)
     );
     this.advisors = Advisers;
-    this.totalQuarters = 10;
+    this.totalQuarters = this.getRandomOption([8,10,15]);
     this.currentQuarter = 0;
-    this.capital = 100;
-    this.capitalPerQuarter = 25;
-    this.actions = 0;
-    this.actionsPerQuarter = 10;
+    this.capital = this.getRandomOption([50,100,200]);
+    this.capitalPerQuarter = this.getRandomOption([15,25,30]);
+    this.actions = this.getRandomOption([1,2,3]);
+    this.actionsPerQuarter = this.getRandomOption([1,2,3]);
     this.satisfactionPerQuarter = [];
   }
 
@@ -49,4 +53,11 @@ export default class CSGame {
     this.capital -= program.onBuyCaptial;
     this.actions -= program.onBuyActionPoints;
   }
+
+  //pass in an array and return 1 object from that array randomly
+  getRandomOption(list){
+    var randomIndex = Math.floor(Math.random() * list.length);
+    return list[randomIndex];
+  }
+
 }
