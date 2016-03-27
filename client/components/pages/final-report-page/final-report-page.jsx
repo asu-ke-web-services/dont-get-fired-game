b0fbbe31c0c783ae15e0ee24abebe5e76b603925
xyan12345 <xuefengyan@yahoo.com>
2016-03-27 00:11:49 -0700

    Fixed final-report page

--- client/components/pages/final-report-page/final-report-page.jsx
+++ client/components/pages/final-report-page/final-report-page.jsx
@@ -18,18 +18,31 @@ const FinalReportPage = React.createClass({
   render() {
     let Win = <div>-</div>;
     if (this.props.state.game.goalsMeet === true) {
-      Win = <div>
-        Your goal was {this.props.state.game.goal}.</div>;
+      Win = <div><p>Congrats!</p>
+      <p>You achieved company's goal!</p>
+      <p>{this.props.state.game.companyName} achieved ${this.props.state.game.goals.capital} cashflow!</p>
+      <p>{this.props.state.game.companyName} had satisfaction:
+      {this.props.state.game.goals.satisfaction}!</p>
+      <p>You did so well! Company wants you be next CEO again.</p>
+      <p>Come back to us!</p></div>;
     }
     if (this.props.state.game.goalsMeet === false) {
-      Win = <div>You Lose... :(</div>;
+      Win = <div><p> Sorry! You failed your company!</p>
+      <p> You are fired </p>
+      <p>The Company should achieve ${this.props.state.game.goals.captial}!</p>
+      <p>Satisfaction should be {this.props.state.game.goals.satisfaction}!</p>
+      <p>Sorry, you lost the game!</p>
+      <p>you can do better next time </p></div>;
     }
 
     return (
       <div className="final-report">
         <h1 className="final-report__headline"> {Win} </h1>
         <span className="final-report__information">
-           <span>Play again? {this.props.state.game.gameOver}</span>
+           <br/>
+           <br/>
+           <br/>
+           <span>Play Again? {this.props.state.game.gameOver}</span>
           <br/>
           <button className="final-report__button" onClick={this.handleTweet}>
             Tweet
